517ccbc373b19511f6262fac8cfa2539352e856b
valentinvisan <valentin.visan.vv@gmail.com>
2026-03-28 14:04:51 +0200

    feat: add PWA support for Android home screen widget
    - Add manifest.json for installable PWA
    - Add service worker for offline support
    - Switch quotes URL to relative path for GitHub Pages
    - Add PWA icons (192x192, 512x512)

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'bq-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './book-quotes.widget/quotes.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(n) { return n !== CACHE_NAME; })
+             .map(function(n) { return caches.delete(n); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).then(function(res) {
+      var clone = res.clone();
+      caches.open(CACHE_NAME).then(function(cache) {
+        cache.put(e.request, clone);
+      });
+      return res;
+    }).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
